470201db6fe4cf745a42c8f6fd1054c7f03026a8
406-06\student <aaa@zzz.com>
2024-09-03 12:07:15 +0900

    投げる　パーティクル

---
+++ Assets/Object/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    private void OnCollisonEnter(Collider collider)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Application.targetFrameRate = 60;
+        Shoot(new Vector3(0, 200, 2000));
+    }
+}
