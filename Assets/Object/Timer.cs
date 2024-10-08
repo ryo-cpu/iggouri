1c251c21a91c4da6bfd22683d6df61e30a825cac
Koutexya <fumitan0129@gmail.com>
2024-10-08 12:23:38 +0900

    諦めて

---
+++ Assets/Object/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float m_fTimer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_fTimer += Time.deltaTime;
+    }
+    void Reset()
+    {
+        m_fTimer = 0f;
+    }
+    float ReturnTime()
+    {
+        return m_fTimer;
+    }
+}
